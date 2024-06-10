8076fe3762c2855c87296b509e966cef096b05a4
kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
2024-06-10 12:01:32 +0900

    SumOfN_UseWhileプロジェクトの追加

---
+++ SumOfN_UseWhile/SumOfN_UseWhile/SumOfN_UseWhile.cpp
@@ -0,0 +1,41 @@
+﻿//==========================================================
+//	1〜Nの総和　while版
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // sscanf(),scanf(),printf()
+// 関数プロトタイプ
+int SumOfN(int n);
+void calcSum(int N);
+
+int main(int argc, char* argv[])
+{
+	int N;
+	if (argc >= 2) {
+		scanf_s(argv[1], "%d", &N);
+		calcSum(N);
+	}
+	else {
+		while (true) {
+			printf("nの値:");
+			scanf_s("%d", &N);
+			calcSum(N);
+		}
+	}
+	return 0;
+}
+void calcSum(int N)
+{
+	int sum = SumOfN(N);
+	printf("1～%dの総和は%d\n", N, sum);
+}
+
+int SumOfN(int n)
+{
+	int sum = 0;
+	int i = 1;
+	while (i <= n) {
+		sum += i;
+		i++;
+	}
+	return sum;
+}
