cda7608810359143cacc003ed4eb2d93e837333f
kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
2024-06-18 14:57:33 +0900

    演習1-13

---
+++ NineTimesNine_Add/NineTimesNine_Add/NineTimesNine_Add.cpp
@@ -0,0 +1,45 @@
+//==========================================================
+//	九九表(加算)を表示する
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // printf()
+
+int main()
+{
+	// ここをコーディングしてください。
+	for (int i = 0; i <= 9; i++)
+	{
+		if (i == 0)
+		{
+			printf("   |");
+			continue;
+		}
+		printf("%3d", i);
+	}
+	printf("\n");
+
+	for (int i = 0; i <= 9; i++)
+	{
+		if (i == 0)
+		{
+			printf("---+");
+			continue;
+		}
+		printf("---");
+	}
+	printf("\n");
+
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			if (j == 1)
+			{
+				printf("%3d|", i);
+			}
+			printf("%3d", i + j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
