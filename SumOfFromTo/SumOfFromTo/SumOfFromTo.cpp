05009b1b80766d0a9fbf68e7cf80af8e8d776741
kmm2459318 <kmm2459318@stu.o-hara.ac.jp>
2024-06-18 14:29:27 +0900

    演習1-9

---
+++ SumOfFromTo/SumOfFromTo/SumOfFromTo.cpp
@@ -0,0 +1,60 @@
+﻿//==========================================================
+//	a〜bの総和
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // sscanf(),scanf(),printf()
+// 関数プロトタイプ
+int SumOfFromTo(int from, int to);
+void calcSum(int a, int b);
+
+int main(int argc, char* argv[])
+{
+	int a, b;
+	if (argc >= 3) {
+		sscanf_s(argv[1], "%d", &a);
+		sscanf_s(argv[2], "%d", &b);
+		calcSum(a, b);
+	}
+	else {
+		while (true) {
+			printf("aの値:");
+			scanf_s("%d", &a);
+			printf("bの値:");
+			scanf_s("%d", &b);
+			calcSum(a, b);
+		}
+	}
+	return 0;
+}
+void calcSum(int a, int b)
+{
+	int sum = SumOfFromTo(a, b);
+	printf("%d～%dの総和は%d\n", a, b, sum);
+}
+
+int SumOfFromTo(int from, int to)
+{
+	// ここをコーディングしてください。
+	// from<to の場合も対応してください。
+	int sum = 0;
+	if (from <= to)
+	{
+		for (int i = from + 1; i < to; i++)
+		{
+			sum = sum + i;
+		}
+	}
+	else 
+	{
+		for (int i = to + 1; i < from; i++)
+		{
+			sum = sum + i;
+		}
+	}
+	
+
+	
+
+	
+	return sum;
+}
